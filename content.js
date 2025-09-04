94e51221d973abd48e65b586b67376605cc8d29f
Tamanna123garg <gargtamanna069@gmail.com>
2025-09-04 09:26:52 +0530

    HTml and CSS

---
+++ content.js
@@ -0,0 +1,8 @@
+function changeContent() {
+  
+    document.getElementById('myHeading').innerHTML = 'New Heading';
+  
+
+    document.getElementById('myParagraph').innerHTML = 'This is the new content of the paragraph.';
+  }
+  
