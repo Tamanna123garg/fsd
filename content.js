e2e612e5d2c3b8e36c6fdfcd2cc3061279d4686d
Tamanna123garg <gargtamanna069@gmail.com>
2025-09-23 15:45:53 +0530

    Arrow Function

--- content.js
+++
@@ -1,8 +0,0 @@
-function changeContent() {
-  
-    document.getElementById('myHeading').innerHTML = 'New Heading';
-  
-
-    document.getElementById('myParagraph').innerHTML = 'This is the new content of the paragraph.';
-  }
-  
